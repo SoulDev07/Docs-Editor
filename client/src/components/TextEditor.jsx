9fe8def9a0ca15d28fb9ce36a736ee588d998a6a
SoulDev07 <mayankravariya@gmail.com>
2024-05-08 12:42:01 +0530

    add toolbar and katex

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,7 +1,23 @@
 // import React from 'react';
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import Quill from 'quill';
-import "quill/dist/quill.snow.css";
+import katex from 'katex';
+import 'quill/dist/quill.snow.css';
+import 'katex/dist/katex.min.css';
+
+window.katex = katex;
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ align: [] }, { list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline", "strike"],
+  ["link", "blockquote", "code-block", "formula"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  ["image", "video"],
+  ["clean"],
+];
 
 function TextEditor() {
   const [quill, setQuill] = useState();
@@ -15,7 +31,12 @@ function TextEditor() {
     const editor = document.createElement("div");
     wrapper.append(editor);
 
-    const q = new Quill(editor, { theme: "snow" });
+    const q = new Quill(editor, {
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      },
+      theme: "snow"
+    });
     setQuill(q);
   }, []);
 
